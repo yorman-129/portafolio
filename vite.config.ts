e93c1cdffe74cd2caff7dfddcaa1bd0b5acfef5d
yorman-129 <yorman_tobon82201@elpoli.edu.co>
2023-08-23 17:17:13 -0500

    Gestionando el dominio personalizado

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "http://www.yorman.me/",
+  base: "https://www.yorman.me/",
 })
