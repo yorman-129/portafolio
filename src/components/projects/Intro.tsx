f050582edb2e0cfaad4550be9d4c82b67a6b5928
yorman-129 <yorman_tobon82201@elpoli.edu.co>
2023-09-21 12:51:17 -0500

    Change issues on code

---
+++ src/components/projects/Intro.tsx
@@ -0,0 +1,14 @@
+import './intro.css'
+export const Intro = () => {
+    return (
+        <div className="container-intro">
+            <h2 className="intro-title">
+                El mundo de mis proyectos
+            </h2>
+            <p className="intro-description">
+            Mi objetivo es fusionar creatividad y tecnología para crear experiencias digitales excepcionales y funcionales. En mi cartera, podrás explorar proyectos que van desde aplicaciones web receptivas hasta sistemas backend altamente escalables, todos ellos representando mi pasión por el desarrollo de software y mi compromiso con la excelencia.
+                <br/>Si estás buscando un desarrollador apasionado que pueda llevar tu visión a la realidad digital, estás en el lugar correcto. ¡Estoy emocionado de colaborar contigo en futuros proyectos emocionantes!
+            </p>
+        </div>
+    )
+}
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,19 +1,21 @@
 import  {useState}  from "react"
 import { NewsApi } from "./news/NewsApi"
 import TicTacToe from "./tictactoe/TicTacToe"
+import { Intro } from "./Intro"
 import "./project.css"
 const Projects = () => {
-    const [project, setProject] = useState(2)
+    const [project, setProject] = useState(0)
     return (
         <div className="container-box" id="projects">
-            <aside className="aside-list">
-                <p style={{ color: '#7b64ff', fontSize: '18px', margin: '0px 0px', padding:'5px' }}><strong>Explora mis proyectos:</strong> <br/> un emocionante juego de 3 en raya🎮, noticias frescas🗞️ y documentación esencial de una de mis API's desarrolladas🧙‍♂️</p>
+            <aside className="aside-list"> 
+                <h3><strong style={{color: '#7b64ff', textAlign:'center', height:'0px'}}>Explora mis proyectos:</strong></h3>
+                <p style={{ color: '#fff', fontSize: '18px', padding:'5px' }}><br/> un emocionante juego de 3 en raya🎮, noticias frescas🗞️ y documentación esencial de una de mis API's desarrolladas🧙‍♂️</p>
                 <li className={`li-item${project == 1 ? "-selected" : ""}`} onClick={() => setProject(1)} > Noticias</li>
                 <li className={`li-item${project == 2 ? "-selected" : ""}`} onClick={() => setProject(2)} > Juego 3 en raya</li>
             </aside>
             <div className="container-projects">
                 
-                {project == 0 && (<h1>Informacion</h1>)}
+                {project == 0 && (<Intro/>)}
                 {project == 1 && (<NewsApi />)}
                 {project == 2 && (<TicTacToe />)}
             </div>
